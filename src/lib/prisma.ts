713e482ca445ad5ffc093cb900018a00c164c074
Ton Huynh <kintonhtt@gmail.com>
2024-09-23 20:16:21 -0700

    use neon host instead of vercel host

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,7 @@ import { Pool } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
 const prismaClientSingleton = () => {
-  const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
+  const neon = new Pool({ connectionString: process.env.DATABASE_URL });
   const adapter = new PrismaNeon(neon);
   return new PrismaClient({ adapter });
 };
